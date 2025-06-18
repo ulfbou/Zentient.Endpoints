ed97f4f02caa9e13b22ac8e90ed35ea1c07ac331
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-06-18 16:55:23 +0200

    build(endpoints-http): Add assembly info and global suppressions
    Introduced `AssemblyInfo.cs` for `Zentient.Endpoints.Http` to grant `InternalsVisibleTo` access for its test project. Also added `GlobalSuppressions.cs` to manage specific code analysis warnings across the assembly.

---
+++ src/Zentient.Endpoints.Http/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+// <copyright file="GlobalSuppressions.cs" company="Zentient Framework Team">
+// Copyright © 2025 Zentient Framework Team. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+[assembly: SuppressMessage("Maintainability", "CA1506:Avoid excessive class coupling", Justification = "Test for a mapper inherently involves coupling to multiple types/namespaces it maps between.")]
