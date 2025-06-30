4b050f87a0e9ad8df38805f9a5d092483cbb4c6b
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-06-30 12:34:12 +0200

    refactor(constants): Update copyright filename in MetadataKeys.cs

--- src/Zentient.Endpoints/Constants/MetadataKeys.cs
+++ src/Zentient.Endpoints/Constants/MetadataKeys.cs
@@ -1,4 +1,4 @@
-// <copyright file="TransportMetadataKeys.cs" company="Zentient Framework Team">
+// <copyright file="MetadataKeys.cs" company="Zentient Framework Team">
 // Copyright © 2025 Zentient Framework Team. All rights reserved.
 // </copyright>
 
