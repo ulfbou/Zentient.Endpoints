6d8ad159a4f6e34ad7bdab09772411c00f1d1349
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-07-01 14:29:26 +0200

    refactor(endpoints-core): Add LINQ using directive
    This commit adds a `using System.Linq;` directive to `EndpointOutcome.cs` and `TransportMetadata.cs`.
    
    This change is a minor refactoring to ensure that LINQ extension methods are available for use within these files, improving code readability and enabling more concise data manipulation.

--- src/Zentient.Endpoints/TransportMetadata.cs
+++ src/Zentient.Endpoints/TransportMetadata.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Reflection.PortableExecutable;
 
 using Microsoft.AspNetCore.Http;
