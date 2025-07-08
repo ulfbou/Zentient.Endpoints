935d0bab46ab645f82a872030f06eabbbdf0e0ff
Ulf Bourelius <159780773+ulfbou@users.noreply.github.com>
2025-07-08 10:28:33 +0200

    feat(http): Add common HTTP content type constants
    Introduced `HttpContentTypeConstants.cs` to centralize common HTTP content type strings like `ApplicationJson`, `ApplicationXml`, `TextPlain`, and `TextHtml`, improving consistency and reducing magic strings.

---
+++ src/Zentient.Endpoints.Http/Constants/HttpContentTypeConstants.cs
@@ -0,0 +1,14 @@
+// <copyright file="NoContentResponse.cs" company="Zentient Framework Team">
+// Copyright © 2025 Zentient Framework Team. All rights reserved.
+// </copyright>
+
+namespace Zentient.Endpoints.Http.Constants
+{
+    public static class HttpContentTypeConstants
+    {
+        public const string ApplicationJson = "application/json";
+        public const string ApplicationXml = "application/xml";
+        public const string TextPlain = "text/plain";
+        public const string TextHtml = "text/html";
+    }
+}
